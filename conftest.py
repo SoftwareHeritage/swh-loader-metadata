b88d3ce8dfc48d785491b900dc62cbcc2c267b2e
Nicolas Dandrimont <nicolas@dandrimont.eu>
2025-03-26 16:14:06 +0100

    Require the swh.journal pytest plugin explicitly

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
-# Copyright (C) 2019-2023  The Software Heritage developers
+# Copyright (C) 2019-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 pytest_plugins = [
+    "swh.journal.pytest_plugin",
     "swh.loader.pytest_plugin",
     "swh.scheduler.pytest_plugin",
     "swh.storage.pytest_plugin",
